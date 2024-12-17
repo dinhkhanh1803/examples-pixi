a5ec1e18c4eba1b32f2b86e810242b940d83b7f8
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-17 17:39:37 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Assets, Container, ParticleContainer, Rectangle, Renderer, Sprite, Texture } from "pixi.js";
+import { Application, Assets, BLEND_MODES, Container, ParticleContainer, Rectangle, Renderer, Sprite, Texture } from "pixi.js";
 
 const app = new Application({ resizeTo: window, backgroundAlpha: 0 });
 document.body.appendChild(app.view as HTMLCanvasElement);
@@ -8,60 +8,63 @@ class DudeSprite extends Sprite {
     direction: number;
     turningSpeed: number;
     speed: number;
-    offset: number;
+
     constructor(texture: Texture) {
         super(texture);
         this.direction = Math.random() * Math.PI * 2;
         this.turningSpeed = Math.random() - 0.8;
-        this.speed = (2 + Math.random() * 2) * 0.2;
-        this.offset = Math.random() * 100;
+        this.speed = 2 + Math.random() * 2;
+
 
         this.anchor.set(0.5);
         this.scale.set(0.8 + Math.random() * 0.2);
 
-        this.x = Math.random() * app.screen.width;
-        this.y = Math.random() * app.screen.height;
+        this.x = Math.floor(Math.random() * app.screen.width);
+        this.y = Math.floor(Math.random() * app.screen.height);
 
         this.tint = Math.random() * 0x808080;
+        this.blendMode = BLEND_MODES.ADD;
     }
 }
 
-const sprite = new ParticleContainer(1000, {
-    scale: true,
-    position: true,
-    rotation: true,
-    uvs: true,
-    alpha: true,
-});
+const background = Sprite.from('https://pixijs.com/assets/bg_rotate.jpg');
+background.width = app.screen.width;
+background.height = app.screen.height;
+app.stage.addChild(background);
 
-app.stage.addChild(sprite);
+const dudeArray: DudeSprite[] = [];
+const totaldudes = 20;
 
-const maggots: DudeSprite[] = [];
 
-const totalSprites = app.renderer instanceof Renderer ? 1000 : 100;
+// Khởi tạo các sprite và thông tin liên quan
+for (let i = 0; i < totaldudes; i++) {
+    const dude = new DudeSprite(Texture.from('https://pixijs.com/assets/flowerTop.png'));
 
-for (let i = 0; i < totalSprites; i++) {
-    const dude = new DudeSprite(Texture.from('https://pixijs.com/assets/maggot_tiny.png'));
-    maggots.push(dude);
-    sprite.addChild(dude);
+    dudeArray.push(dude);
+    app.stage.addChild(dude);
+
 }
 
 const dudeBoundsPadding = 100;
-const dudeBounds = new Rectangle(-dudeBoundsPadding, -dudeBoundsPadding, app.screen.width * 2, app.screen.height * 2);
 
-let tick = 0;
+const dudeBounds = new Rectangle(
+    -dudeBoundsPadding,
+    -dudeBoundsPadding,
+    app.screen.width + dudeBoundsPadding * 2,
+    app.screen.height + dudeBoundsPadding * 2,
+);
 
 app.ticker.add(() => {
-    // iterate through the sprites and update their position
-    for (let i = 0; i < maggots.length; i++) {
-        const dude = maggots[i];
-        dude.scale.y = 0.95 + Math.sin(tick + dude.offset) * 0.05;
-        dude.direction += dude.turningSpeed * 0.01;
-        dude.x += Math.sin(dude.direction) * (dude.speed * dude.scale.y);
-        dude.y += Math.cos(dude.direction) * (dude.speed * dude.scale.y);
-        dude.rotation = -dude.direction + Math.PI;
+    // iterate through the dudes and update the positions
+    for (let i = 0; i < dudeArray.length; i++) {
+        const dude = dudeArray[i];
 
-        // wrap the maggots
+        dude.direction += dude.turningSpeed * 0.01;
+        dude.x += Math.sin(dude.direction) * dude.speed;
+        dude.y += Math.cos(dude.direction) * dude.speed;
+        dude.rotation = -dude.direction - Math.PI / 2;
+
+        // wrap the dudes by testing their bounds...
         if (dude.x < dudeBounds.x) {
             dude.x += dudeBounds.width;
         }
@@ -76,8 +79,4 @@ app.ticker.add(() => {
             dude.y -= dudeBounds.height;
         }
     }
-
-    // increment the ticker
-    tick += 0.1;
-
 });
