28d32d87cad077313eac6d039085af26ed72b99b
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-18 16:57:12 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,92 @@
 import { Application, Assets, Container, filters, Graphics, SimplePlane, Sprite, TextStyle, Texture, Text, RenderTexture, Point, FederatedPointerEvent } from "pixi.js";
 
-const app = new Application({ width: 1024, height: 768, backgroundAlpha: 0 });
+const app = new Application({ width: 1024, height: 768 });
 document.body.appendChild(app.view as HTMLCanvasElement);
 
 //---------------------------------------------------------
+const starTexture = Texture.from('https://pixijs.com/assets/star.png');
+const starAmount = 1000;
+let cameraZ = 0;
+const fov = 20;
+const baseSpeed = 0.025;
+let speed = 0;
+let warpSpeed = 0;
+const starStretch = 5;
+const starBaseSize = 0.05;
 
-const brush = new Graphics().beginFill(0xffffff).drawCircle(0, 0, 50);
+const stars: {
+    sprite: Sprite;
+    z: number;
+    x: number;
+    y: number;
+}[] = [];
 
-// Create a line that will interpolate the drawn points
-const line = new Graphics();
+for (let i = 0; i < starAmount; i++) {
+    const star = {
+        sprite: new Sprite(starTexture),
+        z: 0,
+        x: 0,
+        y: 0,
+    };
 
-Assets.add({
-    alias: 't1',
-    src: 'https://pixijs.com/assets/bg_grass.jpg'
-});
-Assets.add({
-    alias: 't2',
-    src: 'https://pixijs.com/assets/bg_rotate.jpg'
-});
-
-Assets.load(['t1', 't2']).then(setup);
-
-function setup() {
-    const { width, height } = app.screen;
-    const stageSize = { width, height };
-
-    const background = Object.assign(Sprite.from('t1'), stageSize);
-    const imageToReveal = Object.assign(Sprite.from('t2'), stageSize);
-    const renderTexture = RenderTexture.create(stageSize);
-    const renderTextureSprite = new Sprite(renderTexture);
-
-    imageToReveal.mask = renderTextureSprite;
-    app.stage.addChild(background, imageToReveal, renderTextureSprite);
-
-    app.stage.eventMode = 'static';
-    app.stage.hitArea = app.screen;
-    app.stage
-        .on('pointerdown', pointerDown)
-        .on('pointerup', pointerUp)
-        .on('pointerupoutside', pointerUp)
-        .on('pointermove', pointerMove);
-
-    let dragging = false;
-    let lastDrawnPoint: Point | null;;
-
-    function pointerMove({ global: { x, y } }: FederatedPointerEvent) {
-        if (dragging) {
-            brush.position.set(x, y);
-            app.renderer.render(brush, {
-                renderTexture,
-                clear: false,
-                skipUpdateTransform: false,
-            });
-
-            if (lastDrawnPoint) {
-                line.clear()
-                    .lineStyle({ width: 100, color: 0xffffff })
-                    .moveTo(lastDrawnPoint.x, lastDrawnPoint.y)
-                    .lineTo(x, y);
-                app.renderer.render(line, {
-                    renderTexture,
-                    clear: false,
-                    skipUpdateTransform: false,
-                });
-            }
-            lastDrawnPoint = lastDrawnPoint || new Point();
-            lastDrawnPoint.set(x, y);
-        }
-    }
-
-    function pointerDown(event: FederatedPointerEvent) {
-        dragging = true;
-        pointerMove(event);
-    }
-
-    function pointerUp(event: FederatedPointerEvent) {
-        dragging = false;
-        lastDrawnPoint = null;
-    }
+    star.sprite.anchor.x = 0.5;
+    star.sprite.anchor.y = 0.7;
+    randomizeStar(star, true);
+    app.stage.addChild(star.sprite);
+    stars.push(star);
 }
+
+function randomizeStar(star: {
+    sprite: Sprite;
+    z: number;
+    x: number;
+    y: number;
+}, initial?: boolean) {
+    star.z = initial ? Math.random() * 2000 : cameraZ + Math.random() * 1000 + 2000;
+
+    // Calculate star positions with radial random coordinate so no star hits the camera.
+    const deg = Math.random() * Math.PI * 2;
+    const distance = Math.random() * 50 + 1;
+
+    star.x = Math.cos(deg) * distance;
+    star.y = Math.sin(deg) * distance;
+}
+
+
+// Change flight speed every 5 seconds
+setInterval(() => {
+    warpSpeed = warpSpeed > 0 ? 0 : 1;
+}, 5000);
+
+// Listen for animate update
+app.ticker.add((delta) => {
+    // Simple easing. This should be changed to proper easing function when used for real.
+    speed += (warpSpeed - speed) / 20;
+    cameraZ += delta * 10 * (speed + baseSpeed);
+    for (let i = 0; i < starAmount; i++) {
+        const star = stars[i];
+
+        if (star.z < cameraZ) randomizeStar(star);
+
+        // Map star 3d position to 2d with really simple projection
+        const z = star.z - cameraZ;
+
+        star.sprite.x = star.x * (fov / z) * app.renderer.screen.width + app.renderer.screen.width / 2;
+        star.sprite.y = star.y * (fov / z) * app.renderer.screen.width + app.renderer.screen.height / 2;
+
+        // Calculate star scale & rotation.
+        const dxCenter = star.sprite.x - app.renderer.screen.width / 2;
+        const dyCenter = star.sprite.y - app.renderer.screen.height / 2;
+        const distanceCenter = Math.sqrt(dxCenter * dxCenter + dyCenter * dyCenter);
+        const distanceScale = Math.max(0, (2000 - z) / 2000);
+
+        star.sprite.scale.x = distanceScale * starBaseSize;
+        // Star is looking towards center so that y axis is towards center.
+        // Scale the star depending on how fast we are moving, what the stretchfactor is
+        // and depending on how far away it is from the center.
+        star.sprite.scale.y
+            = distanceScale * starBaseSize
+            + (distanceScale * speed * starStretch * distanceCenter) / app.renderer.screen.width;
+        star.sprite.rotation = Math.atan2(dyCenter, dxCenter) + Math.PI / 2;
+    }
+});
