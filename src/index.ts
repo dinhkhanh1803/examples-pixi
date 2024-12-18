879d972872766ae55821ced3aa7edc196750d6ae
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-18 17:47:34 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -4,91 +4,73 @@ const app = new Application({ width: 1024, height: 768, background: '#1099bb' })
 document.body.appendChild(app.view as HTMLCanvasElement);
 
 //---------------------------------------------------------
-// Get the texture for rope.
-const trailTexture = Texture.from('https://pixijs.com/assets/trail.png');
-const historyX: number[] = [];
-const historyY: number[] = [];
+const texture = Texture.from('https://pixijs.com/assets/bunny.png');
+const containerFrame = new Container();
+const bunnyContainer = new Container();
 
-// historySize determines how long the trail will be.
-const historySize = 20;
-// ropeSize determines how smooth the trail will be.
-const ropeSize = 100;
-const points: Point[] = [];
+let screenshot: HTMLAnchorElement | undefined;
 
 
-// Create history array.
-for (let i = 0; i < historySize; i++) {
-    historyX.push(0);
-    historyY.push(0);
+// Take the screenshot and download it
+async function takeScreenshot() {
+    if (screenshot !== undefined) {
+        screenshot.remove();
+    }
+
+    app.stop();
+    const url = await app.renderer.extract.base64(containerFrame);
+
+    screenshot = document.createElement('a');
+
+    document.body.append(screenshot);
+
+    screenshot.style.position = 'fixed';
+    screenshot.style.top = '20px';
+    screenshot.style.right = '20px';
+    screenshot.download = 'screenshot';
+    screenshot.href = url;
+
+    const image = new Image();
+
+    image.width = app.screen.width / 5;
+    image.src = url;
+
+    screenshot.innerHTML = image.outerHTML;
+
+    app.start();
 }
-// Create rope points.
-for (let i = 0; i < ropeSize; i++) {
-    points.push(new Point(0, 0));
-}
-
-
-// Create the rope
-const rope = new SimpleRope(trailTexture, points);
-
-// Set the blendmode
-rope.blendMode = BLEND_MODES.ADD;
-
-app.stage.addChild(rope);
-
-let mouseposition: { x: number, y: number } | null;
 
 app.stage.eventMode = 'static';
 app.stage.hitArea = app.screen;
-app.stage.on('mousemove', (event) => {
-    mouseposition = mouseposition || { x: 0, y: 0 };
-    mouseposition.x = event.global.x;
-    mouseposition.y = event.global.y;
+app.stage.on('pointerdown', takeScreenshot);
+
+for (let i = 0; i < 25; i++) {
+    const bunny = new Sprite(texture);
+
+    bunny.anchor.set(0.5);
+    bunny.x = (i % 5) * 40;
+    bunny.y = Math.floor(i / 5) * 40;
+    bunnyContainer.addChild(bunny);
+}
+
+bunnyContainer.x = 400;
+bunnyContainer.y = 300;
+bunnyContainer.pivot.x = bunnyContainer.width / 2;
+bunnyContainer.pivot.y = bunnyContainer.height / 2;
+
+app.ticker.add((delta) => {
+    bunnyContainer.rotation += 0.01 * delta;
 });
 
-// Listen for animate update
-app.ticker.add(() => {
-    if (!mouseposition) return;
-
-    // Update the mouse values to history
-    historyX.pop();
-    historyX.unshift(mouseposition.x);
-    historyY.pop();
-    historyY.unshift(mouseposition.y);
-    // Update the points to correspond with history.
-    for (let i = 0; i < ropeSize; i++) {
-        const p = points[i];
-
-        // Smooth the curve with cubic interpolation to prevent sharp edges.
-        const ix = cubicInterpolation(historyX, (i / ropeSize) * historySize);
-        const iy = cubicInterpolation(historyY, (i / ropeSize) * historySize);
-
-        p.x = ix;
-        p.y = iy;
-    }
+const style = new TextStyle({
+    fontFamily: 'Roboto',
+    fill: '#fff',
 });
 
-/**
- * Cubic interpolation based on https://github.com/osuushi/Smooth.js
- */
-function clipInput(k: number, arr: number[]) {
-    if (k < 0) k = 0;
-    if (k > arr.length - 1) k = arr.length - 1;
+const screenshotText = new Text('Click To Take Screenshot', style);
 
-    return arr[k];
-}
+screenshotText.x = Math.round((app.screen.width - screenshotText.width) / 2);
+screenshotText.y = Math.round(screenshotText.height / 2);
 
-function getTangent(k: number, factor: number, array: number[]) {
-    return (factor * (clipInput(k + 1, array) - clipInput(k - 1, array))) / 2;
-}
-
-function cubicInterpolation(array: number[], t: number, tangentFactor = 1) {
-    const k = Math.floor(t);
-    const m = [getTangent(k, tangentFactor, array), getTangent(k + 1, tangentFactor, array)];
-    const p = [clipInput(k, array), clipInput(k + 1, array)];
-
-    t -= k;
-    const t2 = t * t;
-    const t3 = t * t2;
-
-    return (2 * t3 - 3 * t2 + 1) * p[0] + (t3 - 2 * t2 + t) * m[0] + (-2 * t3 + 3 * t2) * p[1] + (t3 - t2) * m[1];
-}
+containerFrame.addChild(bunnyContainer);
+app.stage.addChild(screenshotText, containerFrame);
