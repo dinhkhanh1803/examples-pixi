da2dab4bd9d009d25dd93444a2b9fe3e3fdbc602
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-18 22:27:41 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
-import { Application, Assets, Container, filters, Graphics, SimplePlane, Sprite, TextStyle, Texture, Text, RenderTexture, Point, FederatedPointerEvent, SimpleRope, BLEND_MODES } from "pixi.js";
+import { Application, Assets, Container, filters, Graphics, SimplePlane, Sprite, TextStyle, Texture, Text, RenderTexture, Point, FederatedPointerEvent, SimpleRope, BLEND_MODES, AnimatedSprite } from "pixi.js";
 
 const app = new Application({ width: 1024, height: 768, background: '#1099bb' });
 document.body.appendChild(app.view as HTMLCanvasElement);
 
 //---------------------------------------------------------
-let isFlower = true;
-const texture = Texture.from('https://pixijs.com/assets/flowerTop.png');
-const secondTexture = Texture.from('https://pixijs.com/assets/eggHead.png');
 
-const charactor = new Sprite(texture);
+Assets.load('https://pixijs.com/assets/spritesheet/mc.json').then(() => {
+    const explosionTextures = [];
+    let i;
 
-charactor.anchor.set(0.5);
-charactor.position.set(app.screen.width / 2, app.screen.height / 2);
-app.stage.addChild(charactor);
+    for (i = 0; i < 26; i++) {
+        const texture = Texture.from(`Explosion_Sequence_A ${i + 1}.png`);
 
-charactor.eventMode = 'static';
-charactor.cursor = 'pointer';
-charactor.on('pointerdown', () => {
-    isFlower = !isFlower;
-    charactor.texture = isFlower ? texture : secondTexture;
+        explosionTextures.push(texture);
+    }
+
+    for (i = 0; i < 50; i++) {
+        // create an explosion AnimatedSprite
+        const explosion = new AnimatedSprite(explosionTextures);
+
+        explosion.x = Math.random() * app.screen.width;
+        explosion.y = Math.random() * app.screen.height;
+        explosion.anchor.set(0.5);
+        explosion.rotation = Math.random() * Math.PI;
+        explosion.scale.set(0.75 + Math.random() * 0.5);
+        explosion.gotoAndPlay((Math.random() * 26) | 0);
+        app.stage.addChild(explosion);
+    }
 });
-
-app.ticker.add((time) => {
-    charactor.rotation += 0.02 * time;
-})
