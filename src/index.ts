da54149546eb1495cc2f5c6285a2f2bc56757aa8
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-18 17:53:29 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -4,73 +4,15 @@ const app = new Application({ width: 1024, height: 768, background: '#1099bb' })
 document.body.appendChild(app.view as HTMLCanvasElement);
 
 //---------------------------------------------------------
-const texture = Texture.from('https://pixijs.com/assets/bunny.png');
-const containerFrame = new Container();
-const bunnyContainer = new Container();
 
-let screenshot: HTMLAnchorElement | undefined;
+const buddy = Sprite.from('https://pixijs.com/assets/bunny.png');
 
+buddy.anchor.set(0.5);
+buddy.x = app.screen.width / 2;
+buddy.y = app.screen.height / 2;
 
-// Take the screenshot and download it
-async function takeScreenshot() {
-    if (screenshot !== undefined) {
-        screenshot.remove();
-    }
+app.stage.addChild(buddy);
 
-    app.stop();
-    const url = await app.renderer.extract.base64(containerFrame);
-
-    screenshot = document.createElement('a');
-
-    document.body.append(screenshot);
-
-    screenshot.style.position = 'fixed';
-    screenshot.style.top = '20px';
-    screenshot.style.right = '20px';
-    screenshot.download = 'screenshot';
-    screenshot.href = url;
-
-    const image = new Image();
-
-    image.width = app.screen.width / 5;
-    image.src = url;
-
-    screenshot.innerHTML = image.outerHTML;
-
-    app.start();
-}
-
-app.stage.eventMode = 'static';
-app.stage.hitArea = app.screen;
-app.stage.on('pointerdown', takeScreenshot);
-
-for (let i = 0; i < 25; i++) {
-    const bunny = new Sprite(texture);
-
-    bunny.anchor.set(0.5);
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
-    bunnyContainer.addChild(bunny);
-}
-
-bunnyContainer.x = 400;
-bunnyContainer.y = 300;
-bunnyContainer.pivot.x = bunnyContainer.width / 2;
-bunnyContainer.pivot.y = bunnyContainer.height / 2;
-
-app.ticker.add((delta) => {
-    bunnyContainer.rotation += 0.01 * delta;
-});
-
-const style = new TextStyle({
-    fontFamily: 'Roboto',
-    fill: '#fff',
-});
-
-const screenshotText = new Text('Click To Take Screenshot', style);
-
-screenshotText.x = Math.round((app.screen.width - screenshotText.width) / 2);
-screenshotText.y = Math.round(screenshotText.height / 2);
-
-containerFrame.addChild(bunnyContainer);
-app.stage.addChild(screenshotText, containerFrame);
+app.ticker.add((detal) => {
+    buddy.rotation += detal * 0.1;
+})
