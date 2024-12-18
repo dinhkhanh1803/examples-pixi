677123852708f6db5b0cdf0c4dd6250d213f7bb0
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-18 17:37:58 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,94 @@
-import { Application, Assets, Container, filters, Graphics, SimplePlane, Sprite, TextStyle, Texture, Text, RenderTexture, Point, FederatedPointerEvent } from "pixi.js";
+import { Application, Assets, Container, filters, Graphics, SimplePlane, Sprite, TextStyle, Texture, Text, RenderTexture, Point, FederatedPointerEvent, SimpleRope, BLEND_MODES } from "pixi.js";
 
-const app = new Application({ width: 1024, height: 768 });
+const app = new Application({ width: 1024, height: 768, background: '#1099bb' });
 document.body.appendChild(app.view as HTMLCanvasElement);
 
 //---------------------------------------------------------
-const starTexture = Texture.from('https://pixijs.com/assets/star.png');
-const starAmount = 1000;
-let cameraZ = 0;
-const fov = 20;
-const baseSpeed = 0.025;
-let speed = 0;
-let warpSpeed = 0;
-const starStretch = 5;
-const starBaseSize = 0.05;
+// Get the texture for rope.
+const trailTexture = Texture.from('https://pixijs.com/assets/trail.png');
+const historyX: number[] = [];
+const historyY: number[] = [];
 
-const stars: {
-    sprite: Sprite;
-    z: number;
-    x: number;
-    y: number;
-}[] = [];
+// historySize determines how long the trail will be.
+const historySize = 20;
+// ropeSize determines how smooth the trail will be.
+const ropeSize = 100;
+const points: Point[] = [];
 
-for (let i = 0; i < starAmount; i++) {
-    const star = {
-        sprite: new Sprite(starTexture),
-        z: 0,
-        x: 0,
-        y: 0,
-    };
 
-    star.sprite.anchor.x = 0.5;
-    star.sprite.anchor.y = 0.7;
-    randomizeStar(star, true);
-    app.stage.addChild(star.sprite);
-    stars.push(star);
+// Create history array.
+for (let i = 0; i < historySize; i++) {
+    historyX.push(0);
+    historyY.push(0);
 }
-
-function randomizeStar(star: {
-    sprite: Sprite;
-    z: number;
-    x: number;
-    y: number;
-}, initial?: boolean) {
-    star.z = initial ? Math.random() * 2000 : cameraZ + Math.random() * 1000 + 2000;
-
-    // Calculate star positions with radial random coordinate so no star hits the camera.
-    const deg = Math.random() * Math.PI * 2;
-    const distance = Math.random() * 50 + 1;
-
-    star.x = Math.cos(deg) * distance;
-    star.y = Math.sin(deg) * distance;
+// Create rope points.
+for (let i = 0; i < ropeSize; i++) {
+    points.push(new Point(0, 0));
 }
 
 
-// Change flight speed every 5 seconds
-setInterval(() => {
-    warpSpeed = warpSpeed > 0 ? 0 : 1;
-}, 5000);
+// Create the rope
+const rope = new SimpleRope(trailTexture, points);
+
+// Set the blendmode
+rope.blendMode = BLEND_MODES.ADD;
+
+app.stage.addChild(rope);
+
+let mouseposition: { x: number, y: number } | null;
+
+app.stage.eventMode = 'static';
+app.stage.hitArea = app.screen;
+app.stage.on('mousemove', (event) => {
+    mouseposition = mouseposition || { x: 0, y: 0 };
+    mouseposition.x = event.global.x;
+    mouseposition.y = event.global.y;
+});
 
 // Listen for animate update
-app.ticker.add((delta) => {
-    // Simple easing. This should be changed to proper easing function when used for real.
-    speed += (warpSpeed - speed) / 20;
-    cameraZ += delta * 10 * (speed + baseSpeed);
-    for (let i = 0; i < starAmount; i++) {
-        const star = stars[i];
+app.ticker.add(() => {
+    if (!mouseposition) return;
 
-        if (star.z < cameraZ) randomizeStar(star);
+    // Update the mouse values to history
+    historyX.pop();
+    historyX.unshift(mouseposition.x);
+    historyY.pop();
+    historyY.unshift(mouseposition.y);
+    // Update the points to correspond with history.
+    for (let i = 0; i < ropeSize; i++) {
+        const p = points[i];
 
-        // Map star 3d position to 2d with really simple projection
-        const z = star.z - cameraZ;
+        // Smooth the curve with cubic interpolation to prevent sharp edges.
+        const ix = cubicInterpolation(historyX, (i / ropeSize) * historySize);
+        const iy = cubicInterpolation(historyY, (i / ropeSize) * historySize);
 
-        star.sprite.x = star.x * (fov / z) * app.renderer.screen.width + app.renderer.screen.width / 2;
-        star.sprite.y = star.y * (fov / z) * app.renderer.screen.width + app.renderer.screen.height / 2;
-
-        // Calculate star scale & rotation.
-        const dxCenter = star.sprite.x - app.renderer.screen.width / 2;
-        const dyCenter = star.sprite.y - app.renderer.screen.height / 2;
-        const distanceCenter = Math.sqrt(dxCenter * dxCenter + dyCenter * dyCenter);
-        const distanceScale = Math.max(0, (2000 - z) / 2000);
-
-        star.sprite.scale.x = distanceScale * starBaseSize;
-        // Star is looking towards center so that y axis is towards center.
-        // Scale the star depending on how fast we are moving, what the stretchfactor is
-        // and depending on how far away it is from the center.
-        star.sprite.scale.y
-            = distanceScale * starBaseSize
-            + (distanceScale * speed * starStretch * distanceCenter) / app.renderer.screen.width;
-        star.sprite.rotation = Math.atan2(dyCenter, dxCenter) + Math.PI / 2;
+        p.x = ix;
+        p.y = iy;
     }
 });
+
+/**
+ * Cubic interpolation based on https://github.com/osuushi/Smooth.js
+ */
+function clipInput(k: number, arr: number[]) {
+    if (k < 0) k = 0;
+    if (k > arr.length - 1) k = arr.length - 1;
+
+    return arr[k];
+}
+
+function getTangent(k: number, factor: number, array: number[]) {
+    return (factor * (clipInput(k + 1, array) - clipInput(k - 1, array))) / 2;
+}
+
+function cubicInterpolation(array: number[], t: number, tangentFactor = 1) {
+    const k = Math.floor(t);
+    const m = [getTangent(k, tangentFactor, array), getTangent(k + 1, tangentFactor, array)];
+    const p = [clipInput(k, array), clipInput(k + 1, array)];
+
+    t -= k;
+    const t2 = t * t;
+    const t3 = t * t2;
+
+    return (2 * t3 - 3 * t2 + 1) * p[0] + (t3 - 2 * t2 + t) * m[0] + (-2 * t3 + 3 * t2) * p[1] + (t3 - t2) * m[1];
+}
