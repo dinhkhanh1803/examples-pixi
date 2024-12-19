8feb217d6d32f5b7e741a280d297bcbcad95502c
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-19 16:57:32 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -4,60 +4,19 @@ const app = new Application({ width: 1024, height: 768, background: '#1099bb' })
 document.body.appendChild(app.view as HTMLCanvasElement);
 
 //---------------------------------------------------------
-// create a texture from an image path
-const texture = Texture.from('https://pixijs.com/assets/bunny.png');
+// Css style for icons
+const defaultIcon = 'url(\'https://pixijs.com/assets/bunny.png\'),auto';
+const hoverIcon = 'url(\'https://pixijs.com/assets/bunny_saturated.png\'),auto';
 
-// Scale mode for pixelation
-texture.baseTexture.scaleMode = SCALE_MODES.NEAREST;
+// Add custom cursor stylesevents/custom-mouse-icon
+app.renderer.events.cursorStyles.default = defaultIcon;
+app.renderer.events.cursorStyles.hover = hoverIcon;
 
-for (let i = 0; i < 10; i++) {
-    createBunny(Math.floor(Math.random() * app.screen.width), Math.floor(Math.random() * app.screen.height));
-}
 
-function createBunny(x: number, y: number) {
-    // create our little bunny friend..
-    const bunny = new Sprite(texture);
+const sprite = Sprite.from('https://pixijs.com/assets/button.png');
+sprite.anchor.set(.5);
+sprite.position.set(512, 384);
 
-    bunny.eventMode = 'static';
-
-    bunny.cursor = 'pointer';
-
-    bunny.anchor.set(0.5);
-
-    bunny.scale.set(3);
-
-    bunny.on('pointerdown', onDragStart, bunny);
-
-    bunny.x = x;
-    bunny.y = y;
-
-    // add it to the stage
-    app.stage.addChild(bunny);
-}
-
-let dragTarget: Sprite | null = null;
-
-app.stage.eventMode = 'static';
-app.stage.hitArea = app.screen;
-app.stage.on('pointerup', onDragEnd);
-app.stage.on('pointerupoutside', onDragEnd);
-
-function onDragMove(event: FederatedPointerEvent) {
-    if (dragTarget) {
-        dragTarget.parent.toLocal(event.global, undefined, dragTarget.position);
-    }
-}
-
-function onDragStart(this: Sprite) {
-    this.alpha = 0.5;
-    dragTarget = this;
-    app.stage.on('pointermove', onDragMove);
-}
-
-function onDragEnd() {
-    if (dragTarget) {
-        app.stage.off('pointermove', onDragMove);
-        dragTarget.alpha = 1;
-        dragTarget = null;
-    }
-}
+sprite.eventMode = 'static';
+sprite.cursor = 'hover';
+app.stage.addChild(sprite);
