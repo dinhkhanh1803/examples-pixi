dd3482434756c4e1df82e661f733fd8e468c6037
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-19 17:58:24 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,217 @@
-import { Application, DisplayObject, FederatedPointerEvent, Graphics, Text } from "pixi.js";
-
+import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle } from "pixi.js";
+import '@pixi/graphics-extras';
 
 const app = new Application({ width: 1024, height: 768, background: '#1099bb' });
 document.body.appendChild(app.view as HTMLCanvasElement);
 
-//---------------------------------------------------------
-const title = app.stage.addChild(
-    new Text(
-        `Move your mouse slowly over the boxes to
-    see the order of pointerenter, pointerleave,
-    pointerover, pointerout events on each target!`,
-        {
-            fontSize: 16,
-        },
-    ),
-);
+// //---------------------------------------------------------
+// class Projector extends DisplayObject
+// {
+//     content: Container;
+//     originalTransform: Matrix;
+//     boundary: EventBoundary;
+//     constructor()
+//     {
+//         super();
 
-const logs: string[] = [];
-const logText = app.stage.addChild(
-    new Text('', {
-        fontSize: 14,
-    }),
-);
+//         // The content root to be rendered by this camera.
+//         this.content = new Container();
 
-logText.y = 80;
-logText.x = 2;
+//         // Temporary matrix to store the original projection transform.
+//         this.originalTransform = new Matrix();
 
-app.stage.name = 'stage';
+//         // The event boundary that'll map events downstream into the content
+//         // scene.
+//         this.boundary = new EventBoundary(this.content);
 
-// Mount outer black box
-const blackBox = app.stage.addChild(new Graphics().beginFill(0).drawRect(0, 0, 600, 400).endFill());
+//         // Override copyMouseData to apply inverse worldTransform on
+//         // global coords
+//         this.boundary.copyMouseData = (from:FederatedPointerEvent, to:FederatedPointerEvent) =>
+//         {
+//             // Apply default implementation first
+//             EventBoundary.prototype.copyMouseData.call(this.boundary, from, to);
 
-blackBox.name = 'black box';
-blackBox.x = 400;
+//             // Then bring global coords into content's world
+//             this.worldTransform.applyInverse(to.global, to.global);
+//         };
 
-// Mount white box inside the white one
-const whiteBox = blackBox.addChild(new Graphics().beginFill(0xffffff).drawRect(100, 100, 400, 200).endFill());
+//         // Propagate these events down into the content's scene graph!
+//         ['pointerdown', 'pointerup', 'pointermove', 'pointerover', 'pointerout', 'wheel'].forEach((event) =>
+//         {
+//             this.addEventListener(event, (e) => this.boundary.mapEvent(e));
+//         });
 
-whiteBox.name = 'white box';
+//         this.eventMode = 'static';
+//     }
 
-// Enable interactivity everywhere!
-app.stage.eventMode = 'static';
-app.stage.hitArea = app.screen;
-whiteBox.eventMode = 'static';
-blackBox.eventMode = 'static';
+//     // Pass through cursor
+//     get cursor()
+//     {
+//         return this.boundary.cursor;
+//     }
 
-[app.stage, whiteBox, blackBox].forEach((object) => {
-    object.addEventListener('pointerenter', onEvent);
-    object.addEventListener('pointerleave', onEvent);
-    object.addEventListener('pointerover', onEvent);
-    object.addEventListener('pointerout', onEvent);
-});
+//     // eslint-disable-next-line class-methods-use-this
+//     set cursor(value)
+//     {
+//         throw new Error('The camera\'s cursor is derived from its content!');
+//     }
 
+//     // Pass through calculateBounds
+//     calculateBounds()
+//     {
+//         const contentBounds = this.content.getBounds();
 
-function onEvent(e: FederatedPointerEvent) {
-    const type = e.type;
-    const targetName = (e.target as DisplayObject).name;
-    const currentTargetName = (e.currentTarget as DisplayObject).name;
+//         this._bounds.addFrameMatrix(
+//             this.worldTransform,
+//             contentBounds.x,
+//             contentBounds.y,
+//             contentBounds.width,
+//             contentBounds.height,
+//         );
+//     }
 
-    // Add event to top of logs
-    logs.push(`${currentTargetName} received ${type} event (target is ${targetName})`);
+//     // Pass through containsPoint
+//     containsPoint(point)
+//     {
+//         return !!this.boundary.hitTest(point.x, point.y);
+//     }
 
-    if (currentTargetName === 'stage' || type === 'pointerenter' || type === 'pointerleave') {
-        logs.push('-----------------------------------------', '');
-    }
+//     // Render content with projection
+//     render(renderer)
+//     {
+//         renderer.batch.flush();
 
-    // Prevent logs from growing too long
-    if (logs.length > 30) {
-        while (logs.length > 30) {
-            logs.shift();
-        }
-    }
+//         const projectionSystem = renderer.projection;
+//         const renderTextureSystem = renderer.renderTexture;
 
-    // Update logText
-    logText.text = logs.join('\n');
-}
+//         projectionSystem.transform = projectionSystem.transform || new Matrix();
+//         projectionSystem.transform.copyTo(this.originalTransform);
+//         projectionSystem.transform.append(this.worldTransform);
+//         projectionSystem.update(null, null, 1, !renderTextureSystem.current);
+
+//         this.content.render(renderer);
+
+//         renderer.batch.flush();
+
+//         projectionSystem.transform.copyFrom(this.originalTransform);
+//         projectionSystem.update(null, null, 1, !renderTextureSystem.current);
+//     }
+
+//     // updateTransform also updates content's transform
+//     updateTransform()
+//     {
+//         super.updateTransform();
+
+//         this.content.enableTempParent();
+//         this.content.updateTransform();
+//         this.content.disableTempParent(null);
+//     }
+// }
+
+// // The projector
+// const projector = app.stage.addChild(new Projector());
+
+// // Add coordinate axes!
+// projector.content.addChild(
+//     new Graphics()
+//         .lineStyle({ color: 0, alpha: 0.2, width: 2 })
+//         .moveTo(0, -300)
+//         .lineTo(0, 600)
+//         .moveTo(-100, 0)
+//         .lineTo(700, 0),
+// );
+
+// // Construct the star Graphics
+// const stars = [1, 2, 3].map((i) => new Graphics().beginFill(0xffffff, 0.75).drawStar(0, 0, 18 / i, (100 * i) / 2));
+
+// // Place the stars
+// stars[0].x = 0;
+// stars[1].x = 200;
+// stars[2].x = 500;
+
+// // Add stars to the projector
+// projector.content.addChild(...stars);
+
+// // Make projection x+100, y+300
+// projector.x = 100;
+// projector.y = 300;
+// projector.content.hitArea = new Rectangle(-100, -300, app.screen.width, app.screen.height);
+// // Make hit-area cover the whole screen so we can capture
+// // pointermove everywhere!
+// projector.hitArea = projector.content.hitArea;
+// projector.content.eventMode = 'static';
+
+// // Make stars interactive & add wheel handlers
+// stars.forEach((star) =>
+// {
+//     // Make star interactive
+//     star.eventMode = 'static';
+
+//     // Set initial cursor
+//     star.cursor = 'zoom-in';
+
+//     // Add wheel rotation feedback
+//     star.addEventListener('wheel', (e) =>
+//     {
+//         const scroll = Math.sign(e.deltaY) * Math.min(15, Math.abs(e.deltaY));
+
+//         star.rotation += scroll / 100;
+//     });
+
+//     // Add click zoom-in/zoom-out handler
+//     star.addEventListener('click', (e) =>
+//     {
+//         if (star.scale.x === 1)
+//         {
+//             star.scale.set(1.33);
+//             star.cursor = 'zoom-out';
+//         }
+//         else
+//         {
+//             star.scale.set(1);
+//             star.cursor = 'zoom-in';
+//         }
+//     });
+// });
+
+// BitmapFont.from(
+//     'coordinates',
+//     {
+//         fontFamily: 'Roboto',
+//         fontSize: 16,
+//         fill: '#272d37',
+//     },
+//     { chars: ['Global:() Screen-.,', ['0', '9']] },
+// );
+
+// const coordinates = new BitmapText('Global: (0, 0)\nScreen: (0, 0)', {
+//     fontName: 'coordinates',
+// });
+
+// coordinates.x = 110;
+// coordinates.y = 550;
+
+// app.stage.addChild(coordinates);
+
+// projector.content.addEventListener('pointermove', (e) =>
+// {
+//     const global = `(${e.global.x | 0}, ${e.global.y | 0})`;
+//     const screen = `(${e.screen.x | 0}, ${e.screen.y | 0})`;
+
+//     coordinates.text = `Global: ${global}\nScreen: ${screen}`;
+// });
+
+// const description = new Text(
+//     'The (0, 0) world coordinates for the content is located at the center of the first star!'
+//         + '\n  * Mouse wheel over stars to rotate them'
+//         + '\n  * Click to zoom in or out',
+//     {
+//         fontSize: 16,
+//         fontFamily: 'Roboto',
+//         fill: '#272d37',
+//     },
+// );
+
+// description.position.set(110, 12);
+
+// app.stage.addChild(description);
