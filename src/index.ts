27481b35da2951ca67dccf8bcde84bfab98882b6
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-19 15:11:50 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,71 @@
-import { Application, Assets, Container, filters, Graphics, SimplePlane, Sprite, TextStyle, Texture, Text, RenderTexture, Point, FederatedPointerEvent, SimpleRope, BLEND_MODES, AnimatedSprite, TilingSprite, BitmapText } from "pixi.js";
-
+import { Application, Assets, Container, Graphics, SimplePlane, Sprite, TextStyle, Texture, Text, RenderTexture, Point, FederatedPointerEvent, SimpleRope, BLEND_MODES, AnimatedSprite, TilingSprite, BitmapText } from "pixi.js";
+import '@pixi/graphics-extras';
 const app = new Application({ width: 1024, height: 768, background: '#1099bb' });
 document.body.appendChild(app.view as HTMLCanvasElement);
 
 //---------------------------------------------------------
-declare global {
-    interface Window {
-        WebFontConfig?: {
-            google: {
-                families: string[];
-            };
-            active?: () => void;
-        };
-    }
-}
-// Load them google fonts before starting...
-window.WebFontConfig = {
-    google: {
-        families: ['Snippet'],
-    },
-    active() {
-        init();
-    },
-};
 
-/* eslint-disable */
-// include the web-font loader script
-(function() {
-    const wf = document.createElement('script');
-    wf.src = `${
-        document.location.protocol === 'https:' ? 'https' : 'http'
-        }://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js`;
-    wf.type = 'text/javascript';
-    wf.async = true;
-    const s = document.getElementsByTagName('script')[0];
-    if (s.parentNode) {
-        s.parentNode.insertBefore(wf, s);
-    } else {
-        console.error('No parentNode found for the script element.');
-    }
-})();
-/* eslint-enabled */
+const graphics = new Graphics();
 
-function init() {
-    // create some white text using the Snippet webfont
-    const textSample = new Text('PixiJS text using the\ncustom "Snippet" Webfont', {
-        fontFamily: 'Snippet',
-        fontSize: 50,
-        fill: 'white',
-        align: 'left',
-    });
-    textSample.position.set(50, 200);
-    app.stage.addChild(textSample);
+// Rectangle
+graphics.beginFill(0xde3249);
+graphics.drawRect(50, 50, 100, 100);
+graphics.endFill();
+
+// Rectangle + line style 1
+graphics.lineStyle(2, 0xfeeb77, 1);
+graphics.beginFill(0x650a5a);
+graphics.drawRect(200, 50, 100, 100);
+graphics.endFill();
+
+// Circle
+graphics.lineStyle(0); // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
+graphics.beginFill(0xde3249, 1);
+graphics.drawCircle(100, 250, 50);
+graphics.endFill();
+
+// Circle + line style 1
+graphics.lineStyle(2, 0xfeeb77, 1);
+graphics.beginFill(0x650a5a, 1);
+graphics.drawCircle(250, 250, 50);
+graphics.endFill();
+
+// Ellipse + line style 2
+graphics.lineStyle(2, 0xffffff, 1);
+graphics.beginFill(0xaa4f08, 1);
+graphics.drawEllipse(600, 250, 80, 50);
+graphics.endFill();
+
+// draw a shape
+graphics.beginFill(0xff3300);
+graphics.lineStyle(4, 0xffd900, 1);
+graphics.moveTo(50, 350);
+graphics.lineTo(250, 350);
+graphics.lineTo(100, 400);
+graphics.lineTo(50, 350);
+graphics.closePath();
+graphics.endFill();
+
+// draw a rounded rectangle
+graphics.lineStyle(2, 0xff00ff, 1);
+graphics.beginFill(0x650a5a, 0.25);
+graphics.drawRoundedRect(50, 440, 100, 100, 16);
+graphics.endFill();
+
+// draw star
+graphics.lineStyle(2, 0xffffff);
+graphics.beginFill(0x35cc5a, 1);
+if (graphics.drawStar) {
+    graphics.drawStar(360, 370, 5, 50, 25);
 }
+graphics.endFill();
+
+// draw polygon
+const path = [600, 370, 700, 460, 780, 420, 730, 570, 590, 520];
+
+graphics.lineStyle(0);
+graphics.beginFill(0x3500fa, 1);
+graphics.drawPolygon(path);
+graphics.endFill();
+
+app.stage.addChild(graphics);
