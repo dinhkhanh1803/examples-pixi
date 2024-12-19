9f2c911275847ded62ec881df82b91114b38c7ae
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-19 10:41:37 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,25 @@
-import { Application, Assets, Container, filters, Graphics, SimplePlane, Sprite, TextStyle, Texture, Text, RenderTexture, Point, FederatedPointerEvent, SimpleRope, BLEND_MODES, AnimatedSprite } from "pixi.js";
+import { Application, Assets, Container, filters, Graphics, SimplePlane, Sprite, TextStyle, Texture, Text, RenderTexture, Point, FederatedPointerEvent, SimpleRope, BLEND_MODES, AnimatedSprite, TilingSprite } from "pixi.js";
 
 const app = new Application({ width: 1024, height: 768, background: '#1099bb' });
 document.body.appendChild(app.view as HTMLCanvasElement);
 
 //---------------------------------------------------------
 
-Assets.load('https://pixijs.com/assets/spritesheet/0123456789.json').then((spritesheet) => {
-    const textures = [];
-    let i;
+// create a texture from an image path
+const texture = Texture.from('https://pixijs.com/assets/p2.jpeg');
 
-    for (i = 0; i < 10; i++) {
-        const framekey = `0123456789 ${i}.ase`;
-        const texture = Texture.from(framekey);
-        const time = spritesheet.data.frames[framekey].duration;
+const tilingSprite = new TilingSprite(texture, app.screen.width, app.screen.height);
 
-        textures.push({ texture, time });
-    }
+app.stage.addChild(tilingSprite);
 
-    const scaling = 4;
+let count = 0;
 
-    // create a slow AnimatedSprite
-    const slow = new AnimatedSprite(textures);
+app.ticker.add(() => {
+    count += 0.005;
 
-    slow.anchor.set(0.5);
-    slow.scale.set(scaling);
-    slow.animationSpeed = 0.5;
-    slow.x = (app.screen.width - slow.width) / 2;
-    slow.y = app.screen.height / 2;
-    slow.play();
-    app.stage.addChild(slow);
+    tilingSprite.tileScale.x = 2 + Math.sin(count);
+    tilingSprite.tileScale.y = 2 + Math.cos(count);
 
-    // create a fast AnimatedSprite
-    const fast = new AnimatedSprite(textures);
-
-    fast.anchor.set(0.5);
-    fast.scale.set(scaling);
-    fast.x = (app.screen.width + fast.width) / 2;
-    fast.y = app.screen.height / 2;
-    fast.play();
-    app.stage.addChild(fast);
-
-    // start animating
-    app.start();
+    tilingSprite.tilePosition.x += 1;
+    tilingSprite.tilePosition.y += 1;
 });
