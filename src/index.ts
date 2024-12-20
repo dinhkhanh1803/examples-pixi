c40cfcf5ccefbe4c3cd02309fb715972707ca5dd
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-20 13:32:16 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,86 +1,115 @@
-import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point, Assets, filters, BlurFilter, ColorMatrixFilter } from "pixi.js";
+import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point, Assets, filters, BlurFilter, ColorMatrixFilter, Texture, DisplacementFilter } from "pixi.js";
 import '@pixi/graphics-extras';
 
-const app = new Application({ width: 1024, height: 768, background: '#1099bb' });
+const app = new Application({ width: 1024, height: 768 });
 document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
+class MaggotSprite extends Sprite {
+    direction: number;
+    turnSpeed: number;
+    speed: number;
+    original: Point;
+    constructor(texture: Texture) {
+        super(texture);
+
+        this.direction = Math.random() * Math.PI * 2;
+        this.speed = 1;
+        this.turnSpeed = Math.random() - 0.8;
+
+        this.anchor.set(0.5);
+
+        this.x = Math.random() * bounds.width;
+        this.y = Math.random() * bounds.height;
+
+        this.scale.set(1 + Math.random() * 0.3);
+
+        this.original = new Point();
+        this.original.copyFrom(this.scale);
+    }
+}
 app.stage.eventMode = 'static';
-const bg = Sprite.from('https://pixijs.com/assets/bg_rotate.jpg');
-
-bg.anchor.set(0.5);
-
-bg.x = app.screen.width / 2;
-bg.y = app.screen.height / 2;
-
-const filter = new ColorMatrixFilter();
 
 const container = new Container();
 
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
-
-const bgFront = Sprite.from('https://pixijs.com/assets/bg_scene_rotate.jpg');
-
-bgFront.anchor.set(0.5);
-
-container.addChild(bgFront);
-
-const light2 = Sprite.from('https://pixijs.com/assets/light_rotate_2.png');
-
-light2.anchor.set(0.5);
-container.addChild(light2);
-
-const light1 = Sprite.from('https://pixijs.com/assets/light_rotate_1.png');
-
-light1.anchor.set(0.5);
-container.addChild(light1);
-
-const panda = Sprite.from('https://pixijs.com/assets/panda.png');
-
-panda.anchor.set(0.5);
-
-container.addChild(panda);
-
 app.stage.addChild(container);
-app.stage.filters = [filter];
+
+const padding = 100;
+const bounds = new Rectangle(-padding, -padding, app.screen.width + padding * 2, app.screen.height + padding * 2);
+const maggots: MaggotSprite[] = [];
+
+for (let i = 0; i < 20; i++) {
+    const maggot = new MaggotSprite(Texture.from('https://pixijs.com/assets/maggot.png'));
+
+    container.addChild(maggot);
+    maggots.push(maggot);
+}
+
+const displacementSprite = Sprite.from('https://pixijs.com/assets/pixi-filters/displace.png');
+const displacementFilter = new DisplacementFilter(displacementSprite);
+
+
+app.stage.addChild(displacementSprite);
+
+container.filters = [displacementFilter];
+
+displacementFilter.scale.x = 110;
+displacementFilter.scale.y = 110;
+displacementSprite.anchor.set(0.5);
+
+const ring = Sprite.from('https://pixijs.com/assets/pixi-filters/ring.png');
+
+ring.anchor.set(0.5);
+
+ring.visible = false;
+
+app.stage.addChild(ring);
+
+const bg = Sprite.from('https://pixijs.com/assets/bg_grass.jpg');
+
+bg.width = app.screen.width;
+bg.height = app.screen.height;
+
+bg.alpha = 0.4;
+
+container.addChild(bg);
+
+app.stage.on('mousemove', onPointerMove).on('touchmove', onPointerMove);
+
+function onPointerMove(eventData: FederatedPointerEvent) {
+    ring.visible = true;
+
+    displacementSprite.position.set(eventData.data.global.x - 25, eventData.data.global.y);
+    ring.position.copyFrom(displacementSprite.position);
+}
 
 let count = 0;
-let enabled = true;
 
-app.stage.on('pointertap', () => {
-    enabled = !enabled;
-    app.stage.filters = enabled ? [filter] : null;
-});
+app.ticker.add(() => {
+    count += 0.05;
 
-const help = new Text('Click or tap to turn filters on / off.', {
-    fontFamily: 'Arial',
-    fontSize: 12,
-    fontWeight: 'bold',
-    fill: 'white',
-});
+    for (let i = 0; i < maggots.length; i++) {
+        const maggot = maggots[i];
 
-help.y = app.screen.height - 25;
-help.x = 10;
+        maggot.direction += maggot.turnSpeed * 0.01;
+        maggot.x += Math.sin(maggot.direction) * maggot.speed;
+        maggot.y += Math.cos(maggot.direction) * maggot.speed;
 
-app.stage.addChild(help);
+        maggot.rotation = -maggot.direction - Math.PI / 2;
+        maggot.scale.x = maggot.original.x + Math.sin(count) * 0.2;
 
-app.ticker.add((delta) => {
-    bg.rotation += 0.01;
-    bgFront.rotation -= 0.01;
-    light1.rotation += 0.02;
-    light2.rotation += 0.01;
+        // wrap the maggots around as the crawl
+        if (maggot.x < bounds.x) {
+            maggot.x += bounds.width;
+        }
+        else if (maggot.x > bounds.x + bounds.width) {
+            maggot.x -= bounds.width;
+        }
 
-    panda.scale.x = 1 + Math.sin(count) * 0.04;
-    panda.scale.y = 1 + Math.cos(count) * 0.04;
-
-    count += 0.1;
-
-    const { matrix } = filter;
-
-    matrix[1] = Math.sin(count) * 3;
-    matrix[2] = Math.cos(count);
-    matrix[3] = Math.cos(count) * 1.5;
-    matrix[4] = Math.sin(count / 3) * 2;
-    matrix[5] = Math.sin(count / 2);
-    matrix[6] = Math.sin(count / 4);
+        if (maggot.y < bounds.y) {
+            maggot.y += bounds.height;
+        }
+        else if (maggot.y > bounds.y + bounds.height) {
+            maggot.y -= bounds.height;
+        }
+    }
 });
