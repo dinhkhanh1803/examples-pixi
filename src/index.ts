1699dc46b0d27cbfae3675fa30088c5065122a81
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-20 14:09:56 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,50 @@
-import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point, Assets, filters, BlurFilter, ColorMatrixFilter, Texture, DisplacementFilter, WRAP_MODES } from "pixi.js";
+import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point, Assets, filters, BlurFilter, ColorMatrixFilter, Texture, DisplacementFilter, WRAP_MODES, Filter } from "pixi.js";
 import '@pixi/graphics-extras';
 
 const app = new Application({ width: 1024, height: 768 });
 document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
+const background = Sprite.from('https://pixijs.com/assets/bg_grass.jpg');
 
-app.stage.eventMode = 'static';
+background.width = app.screen.width;
+background.height = app.screen.height;
+app.stage.addChild(background);
+
+// NOTE: this shader wont work on old devices where mediump precision is forced in fragment shader
+// because v5 default vertex shader uses `inputSize` in it. Same uniform in fragment and vertex shader
+// cant have different precision :(
+const shaderFrag = `
+precision highp float;
+
+varying vec2 vTextureCoord;
+
+uniform vec2 mouse;
+uniform vec4 inputSize;
+uniform vec4 outputFrame;
+uniform float time;
+
+void main() {
+  vec2 screenPos = vTextureCoord * inputSize.xy + outputFrame.xy;
+  if (length(mouse - screenPos) < 25.0) {
+      gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0) * 0.7; //yellow circle, alpha=0.7
+  } else {
+      // blend with underlying image, alpha=0.5
+      gl_FragColor = vec4( sin(time), (mouse.xy - outputFrame.xy) / outputFrame.zw, 1.0) * 0.5;
+  }
+}
+`;
 
 const container = new Container();
 
+container.filterArea = new Rectangle(100, 100, app.screen.width - 200, app.screen.height - 200);
 app.stage.addChild(container);
+const filter = new Filter(undefined, shaderFrag, {
+    mouse: new Point(),
+});
+container.filters = [filter];
 
-const flag = Sprite.from('https://pixijs.com/assets/pixi-filters/flag.png');
-
-container.addChild(flag);
-flag.x = 100;
-flag.y = 100;
-
-const displacementSprite = Sprite.from('https://pixijs.com/assets/pixi-filters/displacement_map_repeat.jpg');
-// Make sure the sprite is wrapping.
-
-displacementSprite.texture.baseTexture.wrapMode = WRAP_MODES.REPEAT;
-const displacementFilter = new DisplacementFilter(displacementSprite);
-
-displacementFilter.padding = 10;
-
-displacementSprite.position = flag.position;
-
-app.stage.addChild(displacementSprite);
-
-flag.filters = [displacementFilter];
-
-displacementFilter.scale.x = 30;
-displacementFilter.scale.y = 60;
-
-app.ticker.add(() => {
-    // Offset the sprite position to make vFilterCoord update to larger value.
-    // Repeat wrapping makes sure there's still pixels on the coordinates.
-    displacementSprite.x++;
-
-    // Reset x to 0 when it's over width to keep values from going to very huge numbers.
-    if (displacementSprite.x > displacementSprite.width) {
-        displacementSprite.x = 0;
-    }
+app.stage.hitArea = app.screen;
+app.stage.eventMode = 'static';
+app.stage.on('pointermove', (event) => {
+    filter.uniforms.mouse.copyFrom(event.global);
 });
