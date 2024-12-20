371c37e08de31214960891dc745d5eb6cc3ab43f
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-20 15:21:51 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,109 @@
-import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point, Assets, filters, BlurFilter, ColorMatrixFilter, Texture, DisplacementFilter, WRAP_MODES, Filter } from "pixi.js";
+import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point, Assets, filters, BlurFilter, ColorMatrixFilter, Texture, DisplacementFilter, WRAP_MODES, Filter, MIPMAP_MODES } from "pixi.js";
 
 
 const app = new Application({ width: 1024, height: 768 });
 document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
-// Create background image
-const background = Sprite.from('https://pixijs.com/assets/bg_grass.jpg');
-
-background.width = app.screen.width;
-background.height = app.screen.height;
-app.stage.addChild(background);
-
-// Stop application wait for load to finish
-app.stop();
-
-fetch('https://pixijs.com/assets/pixi-filters/shader.frag')
-    .then((res) => res.text())
-    .then(onLoaded);
+Assets.load('https://pixijs.com/assets/perlin.jpg').then(onAssetsLoaded);
 
 let filter: Filter;
 
-// // Handle the load completed
-function onLoaded(data: string) {
-    // Create the new filter, arguments: (vertexShader, framentSource)
-    filter = new Filter(undefined, data, {
-        customUniform: 0.0,
-    });
+const text = new Text('PixiJS', { fill: 0xffffff, fontSize: 80 });
 
-    //     // === WARNING ===
-    //     // specify uniforms in filter constructor
-    //     // or set them BEFORE first use
-    //     // filter.uniforms.customUniform = 0.0
+text.anchor.set(0.5, 0.5);
+text.position.set(app.renderer.screen.width / 2, app.renderer.screen.height / 2);
 
-    //     // Add the filter
-    background.filters = [filter];
+app.stage.addChild(text);
 
-    //     // Resume application update
-    app.start();
+let totalTime = 0;
+
+// Fragment shader, in real use this would be much cleaner when loaded from a file
+// or embedded into the application as data resource.
+const fragment = `//Based on this: https://www.shadertoy.com/view/wtlSWX
+
+varying vec2 vTextureCoord;
+uniform sampler2D uSampler;
+uniform sampler2D noise;
+uniform float time;
+// Distance function. Just calculates the height (z) from x,y plane with really simple length check.
+// Its not exact as there could be shorter distances.
+vec2 dist(vec3 p)
+{
+  float id = floor(p.x)+floor(p.y);
+  id = mod(id, 2.);
+  float h = texture2D(noise, vec2(p.x, p.y)*0.04).r*5.1;
+  float h2 = texture2D(uSampler, vTextureCoord).r;
+  return vec2(h+h2-p.z,id);
+}
+//Light calculation.
+vec3 calclight(vec3 p, vec3 rd)
+{
+  vec2 eps = vec2( 0., 0.001);
+  vec3 n = normalize( vec3(
+    dist(p+eps.yxx).x - dist(p-eps.yxx).x,
+    dist(p+eps.xyx).x - dist(p-eps.xyx).x,
+    dist(p+eps.xxy).x - dist(p-eps.xxy).x
+  ));
+
+  vec3 d = vec3( max( 0., dot( -rd ,n)));
+
+  return d;
 }
 
-// // Animate the filter
-// Cập nhật giá trị của customUniform mỗi frame
-app.ticker.add((delta) => {
-    filter.uniforms.customUniform += 0.04 * delta; // Tăng dần giá trị của customUniform
-});
+void main()
+{
+  vec2 uv = vec2(vTextureCoord.x, 1.-vTextureCoord.y);
+  uv *=2.;
+  uv-=1.;
+
+  vec3 cam = vec3(0.,time -2., -3.);
+  vec3 target = vec3(sin(time)*0.1, time+cos(time)+2., 0. );
+  float fov = 2.2;
+  vec3 forward = normalize( target - cam);
+  vec3 up = normalize(cross( forward, vec3(0., 1.,0.)));
+  vec3 right = normalize( cross( up, forward));
+  vec3 raydir = normalize(vec3( uv.x *up + uv.y * right + fov*forward));
+
+  //Do the raymarch
+  vec3 col = vec3(0.);
+  float t = 0.;
+  for( int i = 0; i < 100; i++)
+  {
+    vec3 p = t * raydir + cam;
+    vec2 d = dist(p);
+    t+=d.x*0.5;//Jump only half of the distance as height function used is not really the best for heightmaps.
+    if(d.x < 0.001)
+    {
+      vec3 bc = d.y < 0.5 ? vec3(1.0, .8, 0.) :
+                vec3(0.8,0.0, 1.0);
+      col = vec3( 1.) * calclight(p, raydir) * (1. - t/150.) *bc;
+      break;
+    }
+    if(t > 1000.)
+    {
+      break;
+    }
+  }
+  gl_FragColor = vec4(col, 1.);
+}
+`;
+
+function onAssetsLoaded(perlin: Texture) {
+    // Add perlin noise for filter, make sure it's wrapping and does not have mipmap.
+    perlin.baseTexture.wrapMode = WRAP_MODES.REPEAT;
+    perlin.baseTexture.mipmap = MIPMAP_MODES.OFF;
+
+    // Build the filter
+    filter = new Filter(undefined, fragment, {
+        time: 0.0,
+        noise: perlin,
+    });
+    app.stage.filterArea = app.renderer.screen;
+    app.stage.filters = [filter];
+
+    // Listen for animate update.
+    app.ticker.add((delta) => {
+        filter.uniforms.time = totalTime;
+        totalTime += delta / 60;
+    });
+}
