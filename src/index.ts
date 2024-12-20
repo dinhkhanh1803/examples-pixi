3086a769dc1a9c42849ac96fd8910f384ca6f680
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-20 15:47:20 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,40 @@
-import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point, Assets, filters, BlurFilter, ColorMatrixFilter, Texture, DisplacementFilter, WRAP_MODES, Filter, MIPMAP_MODES, SimpleRope } from "pixi.js";
+import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point, Assets, filters, BlurFilter, ColorMatrixFilter, Texture, DisplacementFilter, WRAP_MODES, Filter, MIPMAP_MODES, SimpleRope, Geometry, Shader, Mesh } from "pixi.js";
 
 
 const app = new Application({ width: 1024, height: 768 });
 document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
 
-let count = 0;
-// build a rope!
-const ropeLength = 45;
+const geometry = new Geometry().addAttribute('aVertexPosition', [-100, -50, 100, -50, 0, 100]);
 
-const points: Point[] = [];
+const shader = Shader.from(
+    `
 
-for (let i = 0; i < 25; i++) {
-    points.push(new Point(i * ropeLength, 0));
-}
+    precision mediump float;
+    attribute vec2 aVertexPosition;
 
-const strip = new SimpleRope(Texture.from('https://pixijs.com/assets/snake.png'), points);
+    uniform mat3 translationMatrix;
+    uniform mat3 projectionMatrix;
 
-strip.x = -40;
-strip.y = 300;
+    void main() {
+        gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+    }`,
 
-app.stage.addChild(strip);
+    `precision mediump float;
 
-const g = new Graphics();
-
-g.x = strip.x;
-g.y = strip.y;
-app.stage.addChild(g);
-
-
-// start animating
-app.ticker.add(() => {
-    count += 0.1;
-
-    // make the snake
-    for (let i = 0; i < points.length; i++) {
-        points[i].y = Math.sin(i * 0.5 + count) * 30;
-        points[i].x = i * ropeLength + Math.cos(i * 0.3 + count) * 20;
+    void main() {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
     }
-    renderPoints();
+
+`,
+);
+
+const triangle = new Mesh(geometry, shader);
+
+triangle.position.set(400, 300);
+
+app.stage.addChild(triangle);
+
+app.ticker.add((delta) => {
+    triangle.rotation += 0.01;
 });
-
-function renderPoints() {
-    g.clear();
-
-    g.lineStyle(2, 0xffc2c2);
-    g.moveTo(points[0].x, points[0].y);
-
-    for (let i = 1; i < points.length; i++) {
-        g.lineTo(points[i].x, points[i].y);
-    }
-
-    for (let i = 1; i < points.length; i++) {
-        g.beginFill(0xff0022);
-        g.drawCircle(points[i].x, points[i].y, 10);
-        g.endFill();
-    }
-}
