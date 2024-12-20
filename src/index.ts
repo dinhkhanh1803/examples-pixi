e5d082a916a2cf86becb7211fc671837701896a9
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-20 15:42:54 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -6,36 +6,54 @@ document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
 
 let count = 0;
-
 // build a rope!
-const ropeLength = 918 / 20;
+const ropeLength = 45;
 
 const points: Point[] = [];
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 25; i++) {
     points.push(new Point(i * ropeLength, 0));
 }
 
 const strip = new SimpleRope(Texture.from('https://pixijs.com/assets/snake.png'), points);
 
-strip.x = -459;
+strip.x = -40;
+strip.y = 300;
 
-const snakeContainer = new Container();
+app.stage.addChild(strip);
 
-snakeContainer.x = 400;
-snakeContainer.y = 300;
+const g = new Graphics();
 
-snakeContainer.scale.set(800 / 1100);
-app.stage.addChild(snakeContainer);
+g.x = strip.x;
+g.y = strip.y;
+app.stage.addChild(g);
 
-snakeContainer.addChild(strip);
 
+// start animating
 app.ticker.add(() => {
-    count += 0.1;// tốc độ của chuyển động.
+    count += 0.1;
 
     // make the snake
     for (let i = 0; i < points.length; i++) {
         points[i].y = Math.sin(i * 0.5 + count) * 30;
         points[i].x = i * ropeLength + Math.cos(i * 0.3 + count) * 20;
     }
+    renderPoints();
 });
+
+function renderPoints() {
+    g.clear();
+
+    g.lineStyle(2, 0xffc2c2);
+    g.moveTo(points[0].x, points[0].y);
+
+    for (let i = 1; i < points.length; i++) {
+        g.lineTo(points[i].x, points[i].y);
+    }
+
+    for (let i = 1; i < points.length; i++) {
+        g.beginFill(0xff0022);
+        g.drawCircle(points[i].x, points[i].y, 10);
+        g.endFill();
+    }
+}
