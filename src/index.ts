917ff4ebf38a9cadfd11a5a3ba3c5f6c96ff4637
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-20 12:38:41 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,44 @@
-import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point } from "pixi.js";
+import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point, Assets, filters, BlurFilter } from "pixi.js";
 import '@pixi/graphics-extras';
 
 const app = new Application({ width: 1024, height: 768, background: '#1099bb' });
 document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
-app.stage.eventMode = 'static';
 
-const bg = Sprite.from('https://pixijs.com/assets/bg_plane.jpg');
+// Inner radius of the circle
+const radius = 100;
 
-app.stage.addChild(bg);
+// The blur amount
+const blurSize = 32;
 
-const cells = Sprite.from('https://pixijs.com/assets/cells.png');
+Assets.load('https://pixijs.com/assets/bg_grass.jpg').then((grassTexture) => {
+    const background = new Sprite(grassTexture);
 
-cells.scale.set(1.5);
+    app.stage.addChild(background);
+    background.width = app.screen.width;
+    background.height = app.screen.height;
 
-const mask = Sprite.from('https://pixijs.com/assets/flowerTop.png');
+    const circle = new Graphics()
+        .beginFill(0xff0000)
+        .drawCircle(radius + blurSize, radius + blurSize, radius)
+        .endFill();
 
-mask.anchor.set(0.5);
-mask.x = 310;
-mask.y = 190;
+    circle.filters = [new BlurFilter(blurSize)];
 
-cells.mask = mask;
+    const bounds = new Rectangle(0, 0, (radius + blurSize) * 2, (radius + blurSize) * 2);
+    const texture = app.renderer.generateTexture(circle, {
+        resolution: 1, // Độ phân giải
+        region: bounds, // Khu vực cần lấy texture
+    });
+    const focus = new Sprite(texture);
 
-app.stage.addChild(mask, cells);
+    app.stage.addChild(focus);
+    background.mask = focus;
 
-const target = new Point();
-
-reset();
-
-function reset() {
-    target.x = Math.floor(Math.random() * 550);
-    target.y = Math.floor(Math.random() * 300);
-}
-
-app.ticker.add(() => {
-    mask.x += (target.x - mask.x) * 0.1;
-    mask.y += (target.y - mask.y) * 0.1;
-
-    if (Math.abs(mask.x - target.x) < 1) {
-        reset();
-    }
+    app.stage.eventMode = 'static';
+    app.stage.hitArea = app.screen;
+    app.stage.on('pointermove', (event) => {
+        focus.position.x = event.global.x - focus.width / 2;
+        focus.position.y = event.global.y - focus.height / 2;
+    });
 });
