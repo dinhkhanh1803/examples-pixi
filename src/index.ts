b36b8cbe80b237028dd194500107670586fbd479
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-20 10:51:05 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES } from "pixi.js";
+import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point } from "pixi.js";
 import '@pixi/graphics-extras';
 
 const app = new Application({ width: 1024, height: 768, background: '#1099bb' });
@@ -6,90 +6,38 @@ document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
 app.stage.eventMode = 'static';
 
-const bg = Sprite.from('https://pixijs.com/assets/bg_rotate.jpg');
-
-bg.anchor.set(0.5);
-
-bg.x = app.screen.width / 2;
-bg.y = app.screen.height / 2;
+const bg = Sprite.from('https://pixijs.com/assets/bg_plane.jpg');
 
 app.stage.addChild(bg);
 
-const container = new Container();
+const cells = Sprite.from('https://pixijs.com/assets/cells.png');
 
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
+cells.scale.set(1.5);
 
-// add a bunch of sprites
-const bgFront = Sprite.from('https://pixijs.com/assets/bg_scene_rotate.jpg');
+const mask = Sprite.from('https://pixijs.com/assets/flowerTop.png');
 
-bgFront.anchor.set(0.5);
+mask.anchor.set(0.5);
+mask.x = 310;
+mask.y = 190;
 
-const light2 = Sprite.from('https://pixijs.com/assets/light_rotate_2.png');
+cells.mask = mask;
 
-light2.anchor.set(0.5);
+app.stage.addChild(mask, cells);
 
-const light1 = Sprite.from('https://pixijs.com/assets/light_rotate_1.png');
+const target = new Point();
 
-light1.anchor.set(0.5);
+reset();
 
-const panda = Sprite.from('https://pixijs.com/assets/panda.png');
-
-panda.anchor.set(0.5);
-
-container.addChild(bgFront, light2, light1, panda);
-
-app.stage.addChild(container);
-
-const thing = new Graphics();
-
-app.stage.addChild(thing);
-thing.x = app.screen.width / 2;
-thing.y = app.screen.height / 2;
-thing.lineStyle(0);
-
-container.mask = thing;
-
-let count = 0;
-
-app.stage.on('pointertap', () => {
-    if (!container.mask) {
-        container.mask = thing;
-    }
-    else {
-        container.mask = null;
-    }
-});
-
-const help = new Text('Click or tap to turn masking on / off.', {
-    fontFamily: 'Arial',
-    fontSize: 12,
-    fontWeight: 'bold',
-    fill: 'white',
-});
-
-help.y = app.screen.height - 46;
-help.x = 200;
-app.stage.addChild(help);
+function reset() {
+    target.x = Math.floor(Math.random() * 550);
+    target.y = Math.floor(Math.random() * 300);
+}
 
 app.ticker.add(() => {
-    bg.rotation += 0.01;
-    bgFront.rotation -= 0.01;
+    mask.x += (target.x - mask.x) * 0.1;
+    mask.y += (target.y - mask.y) * 0.1;
 
-    light1.rotation += 0.02;
-    light2.rotation += 0.01;
-
-    panda.scale.x = 1 + Math.sin(count) * 0.04;
-    panda.scale.y = 1 + Math.cos(count) * 0.04;
-
-    count += 0.1;
-
-    thing.clear();
-
-    thing.beginFill(0x8bc5ff, 0.4);
-    thing.moveTo(-120 + Math.sin(count) * 20, -100 + Math.cos(count) * 20);
-    thing.lineTo(120 + Math.cos(count) * 20, -100 + Math.sin(count) * 20);
-    thing.lineTo(120 + Math.sin(count) * 20, 100 + Math.cos(count) * 20);
-    thing.lineTo(-120 + Math.cos(count) * 20, 100 + Math.sin(count) * 20);
-    thing.rotation = count * 0.1;
+    if (Math.abs(mask.x - target.x) < 1) {
+        reset();
+    }
 });
