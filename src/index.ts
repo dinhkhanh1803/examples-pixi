84eb7b8486f9801d9adb0e3b4de5d5010b855c5c
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-20 15:56:02 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -15,26 +15,11 @@ const geometry = new Geometry()
             -100, // x, y
             100,
             100,
+            -100,
+            100,
         ], // x, y
         2,
     ) // the size of the attribute
-
-    .addAttribute(
-        'aColor', // the attribute name
-        [
-            1,
-            0,
-            0, // r, g, b
-            0,
-            1,
-            0, // r, g, b
-            0,
-            0,
-            1,
-        ], // r, g, b
-        3,
-    ) // the size of the attribute
-
     .addAttribute(
         'aUvs', // the attribute name
         [
@@ -44,28 +29,28 @@ const geometry = new Geometry()
             0, // u, v
             1,
             1,
+            0,
+            1,
         ], // u, v
         2,
-    ); // the size of the attribute
+    ) // the size of the attribute
+    .addIndex([0, 1, 2, 0, 2, 3]);
 
 const vertexSrc = `
 
     precision mediump float;
 
     attribute vec2 aVertexPosition;
-    attribute vec3 aColor;
     attribute vec2 aUvs;
 
     uniform mat3 translationMatrix;
     uniform mat3 projectionMatrix;
 
     varying vec2 vUvs;
-    varying vec3 vColor;
 
     void main() {
 
         vUvs = aUvs;
-        vColor = aColor;
         gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
 
     }`;
@@ -74,27 +59,35 @@ const fragmentSrc = `
 
     precision mediump float;
 
-    varying vec3 vColor;
     varying vec2 vUvs;
 
     uniform sampler2D uSampler2;
+    uniform float time;
 
     void main() {
 
-        gl_FragColor = texture2D(uSampler2, vUvs) * vec4(vColor, 1.0);
+        gl_FragColor = texture2D(uSampler2, vUvs + sin( (time + (vUvs.x) * 14.) ) * 0.1 );
     }`;
 
-const uniforms = { uSampler2: Texture.from('https://pixijs.com/assets/bg_scene_rotate.jpg') };
+const uniforms = {
+    uSampler2: Texture.from('https://pixijs.com/assets/bg_scene_rotate.jpg'),
+    time: 0,
+};
+
 
 const shader = Shader.from(vertexSrc, fragmentSrc, uniforms);
 
-const triangle = new Mesh(geometry, shader);
+const quad = new Mesh(geometry, shader);
 
-triangle.position.set(400, 300);
-triangle.scale.set(2);
+quad.position.set(400, 300);
+quad.scale.set(2);
 
-app.stage.addChild(triangle);
+app.stage.addChild(quad);
+
+// start the animation..
+// requestAnimationFrame(animate);
 
 app.ticker.add((delta) => {
-    triangle.rotation += 0.01;
+    quad.rotation += 0.01;
+    quad.shader.uniforms.time += 0.1;
 });
