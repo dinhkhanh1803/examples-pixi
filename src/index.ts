3a81e47bc1c1b8db6693d7d94dff8d7260da5603
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-20 16:17:11 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -1,86 +1,75 @@
-import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point, Assets, filters, BlurFilter, ColorMatrixFilter, Texture, DisplacementFilter, WRAP_MODES, Filter, MIPMAP_MODES, SimpleRope, Geometry, Shader, Mesh, Program } from "pixi.js";
+import { Application, Container, DisplayObject, EventBoundary, FederatedPointerEvent, Graphics, Matrix, Text, BitmapFont, BitmapText, Rectangle, Sprite, SCALE_MODES, Point, Assets, filters, BlurFilter, ColorMatrixFilter, Texture, DisplacementFilter, WRAP_MODES, Filter, MIPMAP_MODES, SimpleRope, Geometry, Shader, Mesh, Program, TYPES } from "pixi.js";
 
 
 const app = new Application({ width: 1024, height: 768 });
 document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
 
-const geometry = new Geometry()
-    .addAttribute(
-        'aVertexPosition', // the attribute name
-        [
-            -100,
-            -100, // x, y
-            100,
-            -100, // x, y
-            100,
-            100,
-            -100,
-            100,
-        ], // x, y
-        2,
-    ) // the size of the attribute
-    .addAttribute(
-        'aUvs', // the attribute name
-        [
-            0,
-            0, // u, v
-            1,
-            0, // u, v
-            1,
-            1,
-            0,
-            1,
-        ], // u, v
-        2,
-    ) // the size of the attribute
-    .addIndex([0, 1, 2, 0, 2, 3])
-    .interleave();
+const geometry = new Geometry().addAttribute('aVPos', [-100, 0, 100, 0, 0, -150]);
+
+geometry.instanced = true;
+geometry.instanceCount = 5;
+
+const positionSize = 2;
+const colorSize = 3;
+const buffer = new Float32Array(geometry.instanceCount * (positionSize + colorSize));
+
+geometry.addAttribute('aIPos', buffer, positionSize, false, TYPES.FLOAT, 4 * (positionSize + colorSize), 0, true);
+geometry.addAttribute(
+    'aICol',
+    buffer,
+    colorSize,
+    false,
+    TYPES.FLOAT,
+    4 * (positionSize + colorSize),
+    4 * positionSize,
+    true,
+);
+
+for (let i = 0; i < geometry.instanceCount; i++) {
+    const instanceOffset = i * (positionSize + colorSize);
+
+    buffer[instanceOffset + 0] = i * 80;
+    buffer[instanceOffset + 2] = Math.random();
+    buffer[instanceOffset + 3] = Math.random();
+    buffer[instanceOffset + 4] = Math.random();
+}
 
 const shader = Shader.from(
     `
-
     precision mediump float;
-
-    attribute vec2 aVertexPosition;
-    attribute vec2 aUvs;
+    attribute vec2 aVPos;
+    attribute vec2 aIPos;
+    attribute vec3 aICol;
 
     uniform mat3 translationMatrix;
     uniform mat3 projectionMatrix;
 
-    varying vec2 vUvs;
+    varying vec3 vCol;
 
     void main() {
+        vCol = aICol;
 
-        vUvs = aUvs;
-        gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
-
+        gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVPos + aIPos, 1.0)).xy, 0.0, 1.0);
     }`,
 
     `precision mediump float;
 
-    varying vec2 vUvs;
-
-    uniform sampler2D uSampler2;
+    varying vec3 vCol;
 
     void main() {
-
-        gl_FragColor = texture2D(uSampler2, vUvs );
+        gl_FragColor = vec4(vCol, 1.0);
     }
 
 `,
-    {
-        uSampler2: Texture.from('https://pixijs.com/assets/bg_scene_rotate.jpg'),
-    },
 );
 
-const quad = new Mesh(geometry, shader);
+const triangles = new Mesh(geometry, shader);
 
-quad.position.set(400, 300);
-quad.scale.set(2);
+triangles.position.set(400, 300);
 
-app.stage.addChild(quad);
+app.stage.addChild(triangles);
 
 app.ticker.add((delta) => {
-    quad.rotation += 0.01;
+    triangles.rotation += 0.01;
 });
