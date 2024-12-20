4910324a10f5307f897810c97b6c6e312c6b5af7
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-20 17:36:19 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -5,57 +5,43 @@ const app = new Application({ width: 1024, height: 768 });
 document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
 
-Assets.load('https://pixijs.com/assets/flowerTop.png').then((texture) => {
-    // create rotated textures
-    const textures = [texture];
-    const D8 = groupD8;
+const container = new Container();
 
-    for (let rotate = 1; rotate < 16; rotate++) {
-        const h = D8.isVertical(rotate) ? texture.frame.width : texture.frame.height;
-        const w = D8.isVertical(rotate) ? texture.frame.height : texture.frame.width;
+app.stage.addChild(container);
 
-        const { frame } = texture;
-        const crop = new Rectangle(texture.frame.x, texture.frame.y, w, h);
-        const trim = crop;
-        let rotatedTexture;
+const texture = Texture.from('https://pixijs.com/assets/bunny.png');
 
-        if (rotate % 2 === 0) {
-            rotatedTexture = new Texture(texture.baseTexture, frame, crop, trim, rotate);
-        }
-        else {
-            // HACK to avoid exception
-            // PIXI doesnt like diamond-shaped UVs, because they are different in canvas and webgl
-            rotatedTexture = new Texture(texture.baseTexture, frame, crop, trim, rotate - 1);
-            rotatedTexture.rotate++;
-        }
-        textures.push(rotatedTexture);
-    }
+for (let i = 0; i < 25; i++) {
+    const bunny = new Sprite(texture);
 
-    const offsetX = (app.screen.width / 16) | 0;
-    const offsetY = (app.screen.height / 8) | 0;
-    const gridW = (app.screen.width / 4) | 0;
-    const gridH = (app.screen.height / 5) | 0;
+    bunny.x = (i % 5) * 30;
+    bunny.y = Math.floor(i / 5) * 30;
+    bunny.rotation = Math.random() * (Math.PI * 2);
+    container.addChild(bunny);
+}
 
-    // normal rotations and mirrors
-    for (let i = 0; i < 16; i++) {
-        // create a new Sprite using rotated texture
-        const dude = new Sprite(textures[i < 8 ? i * 2 : (i - 8) * 2 + 1]);
+const rt = RenderTexture.create({
+    width: 300,
+    height: 300,
+    scaleMode: SCALE_MODES.LINEAR,
+    resolution: 1,
+});
 
-        dude.scale.x = 0.5;
-        dude.scale.y = 0.5;
-        // show it in grid
-        dude.x = offsetX + gridW * (i % 4);
-        dude.y = offsetY + gridH * ((i / 4) | 0);
-        app.stage.addChild(dude);
-        const text = new Text(`rotate = ${dude.texture.rotate}`, {
-            fontFamily: 'Courier New',
-            fontSize: '12px',
-            fill: 'white',
-            align: 'left',
-        });
+const sprite = new Sprite(rt);
 
-        text.x = dude.x;
-        text.y = dude.y - 20;
-        app.stage.addChild(text);
-    }
+sprite.x = 450;
+sprite.y = 60;
+app.stage.addChild(sprite);
+
+/*
+ * All the bunnies are added to the container with the addChild method
+ * when you do this, all the bunnies become children of the container, and when a container moves,
+ * so do all its children.
+ * This gives you a lot of flexibility and makes it easier to position elements on the screen
+ */
+container.x = 100;
+container.y = 60;
+
+app.ticker.add(() => {
+    app.renderer.render(container, { renderTexture: rt });
 });
