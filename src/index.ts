af0e7829612f87c4551942efdd184cd027f8c586
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-21 13:16:45 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,84 @@ import { Application, Container, DisplayObject, EventBoundary, FederatedPointerE
 const app = new Application({ width: 1024, height: 768 });
 document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
+async function init() {
+    // manifest example
+    const manifestExample = {
+        bundles: [
+            {
+                name: 'load-screen',
+                assets: [
+                    {
+                        name: 'flowerTop',
+                        srcs: 'https://pixijs.com/assets/flowerTop.png',
+                    },
+                ],
+            },
+            {
+                name: 'game-screen',
+                assets: [
+                    {
+                        name: 'eggHead',
+                        srcs: 'https://pixijs.com/assets/eggHead.png',
+                    },
+                ],
+            },
+        ],
+    };
 
-// Add the assets to load
-Assets.add({ alias: 'flowerTop', src: 'https://pixijs.com/assets/flowerTop.png' });
-Assets.add({ alias: 'eggHead', src: 'https://pixijs.com/assets/eggHead.png' });
+    await Assets.init({ manifest: manifestExample });
 
-// Allow the assets to load in the background
-Assets.backgroundLoad(['flowerTop', 'eggHead']);
+    // bundles can be loaded in the background too!
+    Assets.backgroundLoadBundle(['load-screen', 'game-screen']);
+
+    makeLoadScreen();
+}
 
 
-// If the background load hasn't loaded this asset yet, calling load forces this asset to load now.
-Assets.load('eggHead').then((texture) => {
-    // auxiliar flag for toggling the texture
-    let isEggHead = true;
+async function makeLoadScreen() {
+    // get the assets from the load screen bundle.
+    // If the bundle was already downloaded the promise resolves instantly!
+    const loadScreenAssets = await Assets.loadBundle('load-screen');
 
     // create a new Sprite from the resolved loaded texture
-    const character = new Sprite(texture);
+    const goNext = new Sprite(loadScreenAssets.flowerTop);
 
-    character.anchor.set(0.5);
-    character.x = app.screen.width / 2;
-    character.y = app.screen.height / 2;
-    character.eventMode = 'static';
-    character.cursor = 'pointer';
+    goNext.anchor.set(0.5);
+    goNext.x = app.screen.width / 2;
+    goNext.y = app.screen.height / 2;
+    app.stage.addChild(goNext);
 
-    app.stage.addChild(character);
+    goNext.eventMode = 'static';
+    goNext.cursor = 'pointer';
 
-    character.on('pointertap', async () => {
-        isEggHead = !isEggHead;
-        // These promise are already resolved in the cache.
-        character.texture = await Assets.load(isEggHead ? 'eggHead' : 'flowerTop');
+    goNext.on('pointertap', async () => {
+        goNext.destroy();
+        makeGameScreen();
     });
-});
+}
+
+async function makeGameScreen() {
+    // Wait here until you get the assets
+    // If the user spends enough time in the load screen by the time they reach the game screen
+    // the assets are completely loaded and the promise resolves instantly!
+    const loadScreenAssets = await Assets.loadBundle('game-screen');
+
+    // create a new Sprite from the resolved loaded texture
+    const goBack = new Sprite(loadScreenAssets.eggHead);
+
+    goBack.anchor.set(0.5);
+    goBack.x = app.screen.width / 2;
+    goBack.y = app.screen.height / 2;
+    app.stage.addChild(goBack);
+
+    goBack.eventMode = 'static';
+    goBack.cursor = 'pointer';
+
+    goBack.on('pointertap', async () => {
+        goBack.destroy();
+        // The user can go back and the files are already downloaded
+        makeLoadScreen();
+    });
+}
+
+init();
