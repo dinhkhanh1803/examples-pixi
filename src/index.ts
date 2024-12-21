7f269ce072784052a6a39222e398d8818af4e008
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-21 13:02:55 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,35 @@ import { Application, Container, DisplayObject, EventBoundary, FederatedPointerE
 const app = new Application({ width: 1024, height: 768 });
 document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
-async function init() {
-    try {
-        const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
-        const bunny = Sprite.from(texture);
-        bunny.anchor.set(0.5);
-        bunny.x = app.screen.width / 2;
-        bunny.y = app.screen.height / 2;
-        app.stage.addChild(bunny);
-    } catch (error) {
-        console.error('Error loading texture:', error);
-    }
-}
 
-// Call that async function
-init();
+// Add the assets to load
+Assets.add({ alias: 'flowerTop', src: 'https://pixijs.com/assets/flowerTop.png' });
+Assets.add({ alias: 'eggHead', src: 'https://pixijs.com/assets/eggHead.png' });
+
+// Load the assets and get a resolved promise once both are loaded
+const texturesPromise = Assets.load(['flowerTop', 'eggHead']); // => Promise<{flowerTop: Texture, eggHead: Texture}>
+
+// Load the assets directly with Assets.load
+// const texturesPromise = Assets.load({
+//     flowerTop: 'https://pixijs.com/assets/flowerTop.png',
+//     eggHead: 'https://pixijs.com/assets/eggHead.png'
+// });
+
+// When the promise resolves, we have the texture!
+texturesPromise.then((textures) => {
+    // create a new Sprite from the resolved loaded Textures
+
+    const flower = Sprite.from(textures.flowerTop);
+
+    flower.anchor.set(0.5);
+    flower.x = app.screen.width * 0.25;
+    flower.y = app.screen.height / 2;
+    app.stage.addChild(flower);
+
+    const egg = Sprite.from(textures.eggHead);
+
+    egg.anchor.set(0.5);
+    egg.x = app.screen.width * 0.75;
+    egg.y = app.screen.height / 2;
+    app.stage.addChild(egg);
+});
