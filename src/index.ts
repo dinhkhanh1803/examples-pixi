0df491a2b07bfc2214f0b85fdbaf0a630aee6ca6
dinhkhanh1803 <trandinhkhanh0318@gmail.com>
2024-12-21 12:39:12 +0700

    push

--- src/index.ts
+++ src/index.ts
@@ -4,64 +4,20 @@ import { Application, Container, DisplayObject, EventBoundary, FederatedPointerE
 const app = new Application({ width: 1024, height: 768 });
 document.body.appendChild(app.view as HTMLCanvasElement);
 //---------------------------------------------------------
+// Start loading right away and create a promise
+const texturePromise = Assets.load('https://pixijs.com/assets/bunny.png');
 
-class GradientResource extends Resource {
-    constructor() {
-        // pass width and height. (0,0) if we dont know yet
-        // gradient needs only 1 pixel height
-        super(256, 1);
-    }
+// When the promise resolves, we have the texture!
+texturePromise.then((resolvedTexture) => {
+    // create a new Sprite from the resolved loaded Texture
+    const bunny = Sprite.from(resolvedTexture);
 
-    upload(renderer: Renderer, baseTexture: BaseTexture, glTexture: WebGLTexture) {
-        const { width, height } = baseTexture;
+    // center the sprite's anchor point
+    bunny.anchor.set(0.5);
 
-        // temporary canvas, we dont need it after texture is uploaded to GPU
-        const canvas = document.createElement('canvas');
+    // move the sprite to the center of the screen
+    bunny.x = app.screen.width / 2;
+    bunny.y = app.screen.height / 2;
 
-        canvas.width = width;
-        canvas.height = height;
-
-        const ctx = canvas.getContext('2d');
-        if (ctx) {
-
-
-            const grd = ctx.createLinearGradient(0, 0, width, 0);
-
-            grd.addColorStop(0, 'rgba(255, 255, 255, 0.0)');
-            grd.addColorStop(0.3, 'cyan');
-            grd.addColorStop(0.7, 'red');
-            grd.addColorStop(1, 'green');
-
-            ctx.fillStyle = grd;
-            ctx.fillRect(0, 0, width, height);
-        }
-        // This info ios usseful if upload happens second time
-        // Some people use that to track used memory
-
-
-        // PURE WEBGL CALLS - that's what its all about.
-        // PixiJS cant wrap all that API, we give you acceess to it!
-        const { gl } = renderer;
-        gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
-        gl.texImage2D(baseTexture.target, 0, baseTexture.format, baseTexture.format, baseTexture.type, canvas);
-
-        return true;
-    }
-
-}
-
-
-const gradBaseTexture = new BaseTexture(new GradientResource());
-// Here you can fake baseTexture size to avoid resizing all sprites
-// There can be multiple baseTextures per gradient, but be careful:
-// resource will spawn more glTextures!
-
-gradBaseTexture.setSize(500, 50);
-
-const gradTexture = new Texture(gradBaseTexture);
-
-const sprite = new Sprite(gradTexture);
-
-sprite.position.set(100, 100);
-sprite.rotation = Math.PI / 8;
-app.stage.addChild(sprite);
+    app.stage.addChild(bunny);
+});
